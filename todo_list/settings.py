953d19c988d2eda63c1818fd99aae835554e9620
Ayush Awasthi <20BCS2936@cuchd.in>
2022-05-03 14:41:50 +0530

    update

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-do0y)x%j+yzohh&+(9_%7$nprut#4sgz)px93=y59e5w0+cj0b'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
